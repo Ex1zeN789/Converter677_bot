79d0518f4e87183e9c798a3a8b5906cf771e2ac4
Ex1zeN789 <gorcakovvitalij190@mail.ru>
2026-07-20 22:15:18 +0700

    Prepare Render deployment

--- bot.py
+++ bot.py
@@ -1,14 +1,43 @@
 import asyncio
+import os
+
+from aiohttp import web
 
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
+
 from handlers.start import router
 from handlers.menu import router as menu_router
 from handlers.image_converter import router as image_router
 from handlers.video_converter import router as video_router
 from handlers.background import router as background_router
 from handlers.audio_converter import router as audio_router
+
+
+async def health(request):
+    return web.Response(text="Converter677 Bot is running!")
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", health)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.getenv("PORT", 10000))
+
+    site = web.TCPSite(
+        runner,
+        "0.0.0.0",
+        port
+    )
+
+    await site.start()
+
+    print(f"🌐 Web server started on port {port}")
+
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -21,7 +50,9 @@ async def main():
     dp.include_router(background_router)
     dp.include_router(audio_router)
 
-    print("✅ Бот запускается...")
+    await start_web_server()
+
+    print("✅ Converter677 Bot started!")
 
     await dp.start_polling(bot)
 
